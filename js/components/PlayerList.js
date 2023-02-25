471fe13124622fb19bbf82aab5398b0bc92b853a
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-02-25 14:43:11 +0100

    PlayerList component template and props

--- js/components/PlayerList.js
+++ js/components/PlayerList.js
@@ -2,7 +2,18 @@ import Player from "./Player.js";
 export default{
     components : { Player },
     template: 
-    `
-        <player></player>
-    `
+    `   
+    <section v-show="players.length">
+        <ul class="border border-gray-600 divide-y divide-gray-600">
+            <player 
+            v-for="player in players" 
+            :key="player.id" 
+            :player="player">
+            </player>
+        </ul>
+    </section>
+    `,
+    props:{
+        players: Array
+    }
 };
