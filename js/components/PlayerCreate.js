43990e746c34dea84b3204ed6dc4c38ec4f1de4b
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-02-25 14:29:11 +0100

    PlayerCreate tipo correction

--- js/components/PlayerCreate.js
+++ js/components/PlayerCreate.js
@@ -2,7 +2,7 @@ export default{
     template:
     `
     <form @submit.prevent="addPlayer">
-        <div class"border border-gray-600 text black">
+        <div class="border border-gray-600 text black">
             <input v-model="newPlayer" placeholder="New player in the game" class="p-2">
             <button type="submit" class="bg-white border-l">Add</button>
         </div>
