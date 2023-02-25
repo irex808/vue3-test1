8177c8f8a4aa33d7e6989e9fd1384b32bcc13b25
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-02-25 14:14:31 +0100

    Data ( Array of Players ) added to LeaderBoard component

--- js/components/LeaderBoard.js
+++ js/components/LeaderBoard.js
@@ -8,5 +8,15 @@ export default{
         <player-list></player-list>
         <player-create></player-create>
         
-    `
+    `,
+    data(){
+        return{
+            players: [
+                { name: 'John', score: 10, difference: 0, id: 1 },
+                { name: 'Jack', score: 11, difference: 0, id: 2 },
+                { name: 'Tripple J', score: 12, difference: 0, id: 3 },
+                { name: 'Bowen', score: 13, difference: 0, id: 4 }
+            ]
+        }
+    }
 };
