f918cbfae1af849e9fb347bd03e48499a2c4c83d
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-02-25 14:26:15 +0100

    PlayerCreate form and methods created

--- js/components/PlayerCreate.js
+++ js/components/PlayerCreate.js
@@ -1,5 +1,22 @@
 export default{
     template:
     `
-    `
+    <form @submit.prevent="addPlayer">
+        <div class"border border-gray-600 text black">
+            <input v-model="newPlayer" placeholder="New player in the game" class="p-2">
+            <button type="submit" class="bg-white border-l">Add</button>
+        </div>
+    </form>
+    `,
+    data(){
+        return {
+            newPlayer:''
+        }
+    },
+    methods:{
+        addPlayer(){
+            this.$emit('createPlayer', this.newPlayer);
+            this.newPlayer = '';
+        }
+    }
 };
