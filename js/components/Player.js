b745b9e177f961adcaa18ba2cafbebda8059e894
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-02-25 14:42:03 +0100

    Player component template with props

--- js/components/Player.js
+++ js/components/Player.js
@@ -1,5 +1,13 @@
 export default{
     template:
     `
-    `
+    <section>
+        <li class="p-2 flex justify-between items-center">
+            {{ player.name }}
+        </li>
+    </section>
+    `,
+    props:{
+        player:Object
+    }
 };
