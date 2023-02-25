cf78f30660effcf46fc45f98d4d4819efc8ec3e0
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-02-25 15:58:48 +0100

    LeaderBoard component computed filters method for sorting and methods addPlayer added

--- js/components/LeaderBoard.js
+++ js/components/LeaderBoard.js
@@ -5,8 +5,8 @@ export default{
     components: { PlayerList, PlayerCreate },
     template:
     `
-        <player-list></player-list>
-        <player-create></player-create>
+        <player-list :players="filters.sortPlayersDesc"></player-list>
+        <player-create @createPlayer="addPlayer"></player-create>
         
     `,
     data(){
@@ -16,7 +16,22 @@ export default{
                 { name: 'Jack', score: 11, difference: 0, id: 2 },
                 { name: 'Tripple J', score: 12, difference: 0, id: 3 },
                 { name: 'Bowen', score: 13, difference: 0, id: 4 }
-            ]
+            ],
+            newPlayer:''
+        }
+    },
+    computed:{
+        filters(){
+            return { sortPlayersDesc: this.players.sort((a,b)=>b.score-a.score)}
+        }
+
+    },
+    methods:{
+        addPlayer(name){
+            this.players.push({
+                name: name, score: 15, difference:0, id: this.players.length + 1
+            });
         }
     }
+
 };
