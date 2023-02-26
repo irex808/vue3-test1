856495972c14994ff1b7154eb22a239d2aaf3d45
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-02-26 16:19:42 +0100

    PlayerCreate component added score input field

--- js/components/PlayerCreate.js
+++ js/components/PlayerCreate.js
@@ -3,20 +3,29 @@ export default{
     `
     <form @submit.prevent="addPlayer">
         <div class="border border-gray-600 text black">
-            <input v-model="newPlayer" placeholder="New player in the game" class="p-2">
+            <input v-model="name" placeholder="New player in the game" class="p-2">
+            <input v-model="score" placeholder="Players score" class="p-2">
             <button type="submit" class="bg-white border-l">Add</button>
         </div>
     </form>
     `,
     data(){
         return {
-            newPlayer:''
+            name:'',
+            score:0
         }
     },
     methods:{
         addPlayer(){
-            this.$emit('createPlayer', this.newPlayer);
-            this.newPlayer = '';
+            let score = parseInt(this.score);
+
+            if(isNaN(score)){
+                score = 0;
+            }
+            
+            this.$emit('createPlayer', this.name, score );
+            this.name = '';
+            this.score = 0;
         }
     }
 };
