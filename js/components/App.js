6d1486d97f10c6e78e58cebce275e478a7309947
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-02-28 16:15:20 +0100

    Last commit before passing SFC

--- js/components/App.js
+++ js/components/App.js
@@ -1,8 +1,10 @@
 import LeaderBoard from "./LeaderBoard.js";
+import MoveUpSlotAnimation from "./MoveUpSlotAnimation.js";
 export default {
-    components: { LeaderBoard },
+    components: { LeaderBoard, MoveUpSlotAnimation },
     template: 
     `
     <leader-board></leader-board>
+    <move-up-slot-animation></move-up-slot-animation>
     `
 };
