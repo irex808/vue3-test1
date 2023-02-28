f5452c0bf52047ff07071e6fdeff63b3a7bac39c
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-02-28 11:42:32 +0100

    ScoreButton component created TODO

--- js/components/ScoreButton.js
+++ js/components/ScoreButton.js
@@ -1,5 +1,16 @@
 export default{
     template:
     `
-    `
+    <button v-model="id" @click="score">Smash the score UP !</button>
+    `,
+
+    props:{
+        id: Number
+    },
+    methods:{
+        score(){
+            this.$emit('scoreUp', this.id)
+        }
+    }
+
 }
