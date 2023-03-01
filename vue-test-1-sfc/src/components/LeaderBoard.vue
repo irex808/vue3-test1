b18f132001c413e2381fd133d89dbcb3bda5c1ff
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-03-01 15:03:17 +0100

    LeaderBoard component added flash news when new player is added

--- vue-test-1-sfc/src/components/LeaderBoard.vue
+++ vue-test-1-sfc/src/components/LeaderBoard.vue
@@ -1,66 +1,83 @@
 <script>
 import PlayerList from "./PlayerList.vue";
 import PlayerCreate from "./PlayerCreate.vue";
+import NewPlayerAdded from "./NewPlayerAdded.vue";
 
-export default{
-    components: { PlayerList, PlayerCreate },
-    data(){
-        return{
-            players: [
-                { name: 'John', score: 10, difference: 0, id: 1, position: 0 },
-                { name: 'Jack', score: 11, difference: 0, id: 2, position: 0 },
-                { name: 'Tripple J', score: 12, difference: 0, id: 3, position: 0 },
-                { name: 'Bowen', score: 13, difference: 0, id: 4, position: 0 }
-            ],
-            newPlayer:''
+export default {
+  components: { PlayerList, PlayerCreate, NewPlayerAdded },
+  data() {
+    return {
+      players: [
+        { name: "John", score: 10, difference: 0, id: 1, position: 0 },
+        { name: "Jack", score: 11, difference: 0, id: 2, position: 0 },
+        { name: "Tripple J", score: 12, difference: 0, id: 3, position: 0 },
+        { name: "Bowen", score: 13, difference: 0, id: 4, position: 0 },
+      ],
+      newPlayer: "",
+      open: false,
+    };
+  },
+  computed: {
+    sortPlayersDesc() {
+      let numberOfPlayers = this.players.length;
+      this.players.sort((a, b) => b.score - a.score);
+      for (let i = 0; i < numberOfPlayers; i++) {
+        this.players[i].position = i + 1;
+      }
+      let index = 0;
+      for (let i = 0; i < numberOfPlayers; i++) {
+        if (index === 0) {
+          this.players[i].difference = 0;
+          index++;
+          i--;
+          continue;
         }
+        if (index < numberOfPlayers) {
+          this.players[index].difference =
+            this.players[i].score - this.players[index].score;
+        }
+        if (index === numberOfPlayers - 1) {
+          break;
+        }
+        index++;
+      }
+      return this.players;
     },
-    computed: {
-        sortPlayersDesc() {
-            let numberOfPlayers = this.players.length;
-              this.players.sort((a, b) => b.score - a.score);
-              for(let i = 0; i < numberOfPlayers; i++)
-              {
-                this.players[i].position = i+1;
-              }
-              let index = 0;
-              for(let i = 0; i < numberOfPlayers; i++){
-                if (index === 0) {
-                    this.players[i].difference = 0;
-                    index++;
-                    i--;
-                    continue;
-                }
-                if (index < numberOfPlayers ) {
-                    this.players[index].difference = this.players[i].score - this.players[index].score;
-                }
-                if(index === numberOfPlayers - 1){
-                    break;
-                }
-                index++;
-              }
-              return this.players;
-        }
+  },
+  methods: {
+    addPlayer(name, score) {
+      this.players.push({
+        name: name,
+        score: score,
+        difference: 0,
+        id: this.players.length + 1,
+      });
+      this.showNewPlayer();
     },
-    methods:{
-        addPlayer(name, score){
-            this.players.push({
-                name: name, score: score, difference:0, id: this.players.length+1
-            });
-        },
-        incrementResult(id){
-            for(let i = 0; i < this.players.length; i++){
-                if(this.players[i].id === id){
-                    this.players[i].score++;
-                }
-            }
-            // sortPlayersDesc;
+    incrementResult(id) {
+      for (let i = 0; i < this.players.length; i++) {
+        if (this.players[i].id === id) {
+          this.players[i].score++;
         }
-    }
-
+      }
+      // sortPlayersDesc;
+    },
+    showNewPlayer() {
+      this.open = true;
+      setTimeout(() => (this.open = false), 3000);
+    },
+  },
 };
 </script>
 <template>
-    <PlayerList :players="sortPlayersDesc" @scoreList="incrementResult"></PlayerList>
-    <PlayerCreate @createPlayer="addPlayer"></PlayerCreate>
+  <div>
+    <NewPlayerAdded v-if="this.open"></NewPlayerAdded>
+    <PlayerList
+      :players="sortPlayersDesc"
+      @scoreList="incrementResult"
+    ></PlayerList>
+    <div class="mx-2">
+      <PlayerCreate @createPlayer="addPlayer"></PlayerCreate>
+    </div>
+  </div>
 </template>
