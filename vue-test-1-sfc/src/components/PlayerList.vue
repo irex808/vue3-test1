7214fd3d2a745db4c808c5fa292cda5c08db75d9
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-03-01 11:44:58 +0100

    PlayerList Component added watcher for animation

--- vue-test-1-sfc/src/components/PlayerList.vue
+++ vue-test-1-sfc/src/components/PlayerList.vue
@@ -1,37 +1,49 @@
 <script>
 import Player from "./Player.vue";
-export default{
-    components : { Player },
-    updated(){
-        alert('Player has moved one slot up!');
+import Animation from "./Animation.vue";
+export default {
+  components: { Player, Animation },
+  watch: {
+
+    players: {
+      handler(newChanged, oldChanged) {
+        console.log("It had changed!");
+      },
+      deep: true,
     },
-    data(){
-        return {
-            id: 0
-        }
+  },
+  data() {
+    return {
+      id: 0,
+      namen: "",
+      open: false,
+      changed: this.players,
+    };
+  },
+  props: {
+    players: Array,
+    name: "",
+  },
+  methods: {
+    score(id, name) {
+      this.id = id;
+      this.namen = name;
+      this.$emit("scoreList", this.id);
     },
-    props:{
-        players: Array
-    },
-    methods:{
-        score(id){
-            this.id = id;
-            console.log('PlayerList: ' + this.id);
-            this.$emit('scoreList', this.id);
-        }
-    }
-}
+  },
+};
 </script>
 <template>
-    <section v-show="players.length">
-        <ul class="border border-gray-600 divide-y divide-gray-600">
-            <Player 
-            v-for="player in players" 
-            :key="player.id" 
-            :player="player"
-            @score="score"
-            >
-            </Player>
-        </ul>
-    </section>
+  <section v-show="players.length">
+    <ul class="border border-gray-600 divide-y divide-gray-600">
+      <Player
+        v-for="player in players"
+        :key="player.id"
+        :player="player"
+        @score="score"
+      >
+      </Player>
+    </ul>
+  </section>
 </template>
+<!-- <Animation v-if="this.open"></Animation> -->
