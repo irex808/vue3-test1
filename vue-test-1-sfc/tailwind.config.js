925d8fc104d46bd4e19c0f8affb4a6cb1c928d10
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-03-03 15:18:57 +0100

    Tailwind Container class extended

--- vue-test-1-sfc/tailwind.config.js
+++ vue-test-1-sfc/tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      }
+    },
+    
   },
   variants: {
     extend: {},
