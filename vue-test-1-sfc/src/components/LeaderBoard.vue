c16d251b740d8e7829de66edef1cc1d6f1525877
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-03-03 03:36:06 +0100

    LeaderBoard component sorting and positioning in order now

--- vue-test-1-sfc/src/components/LeaderBoard.vue
+++ vue-test-1-sfc/src/components/LeaderBoard.vue
@@ -8,34 +8,52 @@ export default {
   data() {
     return {
       players: [
-        { name: "John", score: 10, difference: 0, id: 1, position: 0 },
-        { name: "Jack", score: 11, difference: 0, id: 2, position: 0 },
-        { name: "Tripple J", score: 12, difference: 0, id: 3, position: 0 },
-        { name: "Bowen", score: 13, difference: 0, id: 4, position: 0 },
+        {
+          id: 1,
+          player: { name: "John", score: 13, difference: 0, position: 0 },
+        },
+        {
+          id: 2,
+          player: { name: "Jack", score: 12, difference: 0, position: 0 },
+        },
+        {
+          id: 3,
+          player: { name: "Tripple J", score: 11, difference: 0, position: 0 },
+        },
+        {
+          id: 4,
+          player: { name: "Bowen", score: 10, difference: 0, position: 0 },
+        },
+      ],
+      positions: [
+        { id: 1, indexPosition: 0 },
+        { id: 2, indexPosition: 1 },
+        { id: 3, indexPosition: 2 },
+        { id: 4, indexPosition: 3 },
       ],
       newPlayer: "",
       open: false,
-      message: ''
+      message: "",
     };
   },
   computed: {
-    sortPlayersDesc() {
+    sortPlayersAsc() {
       let numberOfPlayers = this.players.length;
-      this.players.sort((a, b) => b.score - a.score);
+      this.sortPlayers();
       for (let i = 0; i < numberOfPlayers; i++) {
-        this.players[i].position = i + 1;
+        this.players[i].player.position = i + 1;
       }
       let index = 0;
       for (let i = 0; i < numberOfPlayers; i++) {
         if (index === 0) {
-          this.players[i].difference = 0;
+          this.players[i].player.difference = 0;
           index++;
           i--;
           continue;
         }
         if (index < numberOfPlayers) {
-          this.players[index].difference =
-            this.players[i].score - this.players[index].score;
+          this.players[index].player.difference =
+            this.players[i].player.score - this.players[index].player.score;
         }
         if (index === numberOfPlayers - 1) {
           break;
@@ -46,25 +64,71 @@ export default {
     },
   },
   methods: {
+    sortPlayers() {
+      this.players.sort((a, b) => b.player.score - a.player.score);
+    },
     addPlayer(name, score) {
+      let idNew = this.players.length + 1;
       this.players.push({
-        name: name,
-        score: score,
-        difference: 0,
-        id: this.players.length + 1,
+        id: idNew,
+        player: {
+          name: name,
+          score: score,
+          difference: 0,
+        },
       });
-      this.showNewPlayer(name);
+      this.sortPlayers();
+      let positionNew = this.findCurrentPosition(idNew, this.players)
+      this.positions.push({
+        id: idNew,
+        indexPosition: positionNew,
+      });
+
+      let words = " is a new Player!";
+      this.showFlashMessage(name, words);
     },
     incrementResult(id) {
       for (let i = 0; i < this.players.length; i++) {
         if (this.players[i].id === id) {
-          this.players[i].score++;
+          this.players[i].player.score++;
+          this.sortPlayers();
+
+          let currentPlayerPosition = this.findCurrentPosition(id, this.players);
+
+          let oldPlayerPositionObj = this.positions.find((el) => el.id === id);
+          let oldPlayerPosition = oldPlayerPositionObj.indexPosition;
+
+          if (currentPlayerPosition !== oldPlayerPosition) {
+            for (let j = 0; j < this.players.length; j++) {
+              let playerId = this.players[j].id;
+
+              let oldPlayerIdPositionObj = this.positions.find((el) => el.id === playerId);
+              let playerIdOldPosition = oldPlayerIdPositionObj.indexPosition;
+
+              if (j !== playerIdOldPosition) {
+                const idPosition = (el) => {
+                  if (el.id === playerId) {
+                    el.indexPosition = j;
+                  }
+                  return el;
+                };
+                let stare = this.positions.filter(idPosition);
+              }
+            }
+            let words = " has moved up on a board!";
+            this.showFlashMessage(this.players[currentPlayerPosition].player.name, words);
+          }
         }
       }
     },
-    showNewPlayer(name) {
+    findCurrentPosition(id, list) {
+      const indexOfId = (el) => el.id === id;
+      let positionNew = list.findIndex(indexOfId);
+      return positionNew;
+    },
+    showFlashMessage(name, words) {
       this.open = true;
-      this.message = name + ' is a new player!'
+      this.message = name + words;
       setTimeout(() => (this.open = false), 3000);
     },
   },
@@ -74,8 +138,8 @@ export default {
   <div>
     <FlashMessage v-if="this.open" :message="message"></FlashMessage>
     <PlayerList
-      :players="sortPlayersDesc"
-      @scoreList="incrementResult"
+      :players="sortPlayersAsc"
+      @onScoreList="incrementResult"
     ></PlayerList>
     <div class="mx-2">
       <PlayerCreate @createPlayer="addPlayer"></PlayerCreate>
