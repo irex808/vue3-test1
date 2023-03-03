4f394425fea702973904cf1a8856488b793beb3a
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-03-03 16:41:02 +0100

    Player component modified css grid alignment

--- vue-test-1-sfc/src/components/Player.vue
+++ vue-test-1-sfc/src/components/Player.vue
@@ -22,7 +22,7 @@ export default{
         <section>
         <li class="p-2 h-28 grid grid-cols-4 content-start justify-items-center">
             <div class="pt-8"> {{ player.player.name }} </div>     <div class="pt-8">{{ player.player.score }}</div>     <div class="pt-8"> {{ player.player.difference }} </div>
-            <div class="justify-right px-3 py-3"> <button class="border border-gray-300 rounded px-2 py-2" @click="score">Smash UP !</button> </div>
+            <div class="justify-right px-3 py-6"> <button class="border border-gray-300 rounded px-2 py-2" @click="score">Smash UP !</button> </div>
         </li>
     </section>
 </template>
