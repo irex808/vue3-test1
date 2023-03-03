cd74f55f4074e128c1d5e41ade3f5b221ce4e07e
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-03-03 03:37:30 +0100

    Player component adjusted for changes made in PlayerList component

--- vue-test-1-sfc/src/components/Player.vue
+++ vue-test-1-sfc/src/components/Player.vue
@@ -1,22 +1,9 @@
 <script>
-import { store } from './store.js'
 export default{
-    watch: {
-    'player.position': {
-      handler(newChanged) {
-        store.count++;
-        if(store.count % 2 === 0){
-            this.changePosition();
-        }
-      },
-      deep: true,
-    },
-  },
     data(){
         return{
             id:0,
-            name:'',
-            store
+            name:''
         }
     },
     props:{
@@ -24,13 +11,9 @@ export default{
     },
     methods:{
         score(){
-            this.id = this.player.id
-            this.name = this.player.name
-            this.$emit('score', this.id, this.name )
-        },
-        changePosition(){
+            this.id = this.player.id;
             this.name = this.player.name;
-            this.$emit('positionUp', this.name);
+            this.$emit('scoreChanged', this.id, this.name )
         }
     }
 }
@@ -38,7 +21,7 @@ export default{
 <template>
         <section>
         <li class="p-2 flex justify-between items-center">
-            {{ player.name }}     <span>{{ player.score }}</span>       {{ player.difference }} 
+            {{ player.player.name }}     <span>{{ player.player.score }}</span>       {{ player.player.difference }} 
             <div class="justify-right px-3 py-3"> <button class="border border-gray-300 rounded px-2 py-2" @click="score">Smash the score UP !</button> </div>
         </li>
     </section>
