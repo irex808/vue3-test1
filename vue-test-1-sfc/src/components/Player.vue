9100e5e52b562db448ecfe44fc4bf87b5f6efbce
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-03-03 16:30:57 +0100

    Player component modified css

--- vue-test-1-sfc/src/components/Player.vue
+++ vue-test-1-sfc/src/components/Player.vue
@@ -20,8 +20,8 @@ export default{
 </script>
 <template>
         <section>
-        <li class="p-2 grid grid-cols-4  align-middle justify-items-center">
-            <span class="pt-8"> {{ player.player.name }} </span>     <span class="pt-8">{{ player.player.score }}</span>     <span class="pt-8"> {{ player.player.difference }} </span>
+        <li class="p-2 h-28 grid grid-cols-4 content-start justify-items-center">
+            <div class="pt-8"> {{ player.player.name }} </div>     <div class="pt-8">{{ player.player.score }}</div>     <div class="pt-8"> {{ player.player.difference }} </div>
             <div class="justify-right px-3 py-3"> <button class="border border-gray-300 rounded px-2 py-2" @click="score">Smash UP !</button> </div>
         </li>
     </section>
