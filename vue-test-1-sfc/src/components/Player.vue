caae3665f18920a27e69d0d61ae4c294c898e7b8
Irinej Stjepanovic <irinej.stjepanovic@beliven.com>
2023-03-03 15:53:11 +0100

    Player component Centering items vertically

--- vue-test-1-sfc/src/components/Player.vue
+++ vue-test-1-sfc/src/components/Player.vue
@@ -21,7 +21,7 @@ export default{
 <template>
         <section>
         <li class="p-2 grid grid-cols-4  align-middle justify-items-center">
-            <span class="pt-6"> {{ player.player.name }} </span>     <span class="pt-6">{{ player.player.score }}</span>     <span class="pt-6"> {{ player.player.difference }} </span>
+            <span class="pt-8"> {{ player.player.name }} </span>     <span class="pt-8">{{ player.player.score }}</span>     <span class="pt-8"> {{ player.player.difference }} </span>
             <div class="justify-right px-3 py-3"> <button class="border border-gray-300 rounded px-2 py-2" @click="score">Smash UP !</button> </div>
         </li>
     </section>
